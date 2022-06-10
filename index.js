c7b61b1f2870bff582a6582414ca54222f130566
tmoino <tony.moino@student.moringaschool.com>
2022-06-10 13:19:34 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = 'Tony'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'billy'
+}
+
+// the error is because you cannot overwrite a variable declare with const
